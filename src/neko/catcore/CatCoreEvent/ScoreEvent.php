18cf625b4539ea67939f1d8666b83378ba28b5e5
Nekiechan <nekiechan678@gmail.com>
2017-05-06 16:22:20 -0500

    Update ScoreEvent.php

--- src/neko/catcore/CatCoreEvent/ScoreEvent.php
+++ src/neko/catcore/CatCoreEvent/ScoreEvent.php
@@ -3,7 +3,55 @@ namespace neko\catcore\CatCoreEvent\ScoreEvent;
 use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\event\player;
+use pocketmine\event\plugin\PluginEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\TranslationContainer;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\event\player\PlayerJoinEvent;
 
-class ScoreEvent {
-  
-  }
+class ScoreEvent extends PluginEvent {
+    public static $handlerList = null;
+    private $score;
+    private $name;
+    private $plrscore;
+    private $scoreConfig;
+    private $scoreConfigdir;
+    public function __construct($plugin, $score, $name, $plrscore, $scoreConfig, $scoreConfigdir){
+        parent::__construct($plugin);
+        $this->player = $name;
+        $this->score = $score;
+        $this->scoredir = $scoreConfigdir;
+        $this->scoreConfig = $scoreConfig;
+    }
+    /**@return Player*/
+    public function getPlayer(){
+        return $this->player;
+    }
+    /**@return int*/
+    public function getScore(){
+        return $this->score;
+    }
+    /**@param int $number*/
+    public function setScore($number){
+        $this->score = $number;
+    }
+    /**@param int $number*/
+    public function addScore($number){
+        if($this->score!==null){
+          $this->score += $number;
+          }else{
+          /** If score is undefined */
+          
+          }
+    }
+    /**@return int*/
+    public function ScoretoPlayer($player, $number){
+        
+    }
+    public function saveScore(){
+    
+    }
+}
