6fac646f793efa052c499f2fd22573a7bd8f08ff
Nekiechan <nekiechan678@gmail.com>
2017-05-06 09:18:39 -0500

    Update nekocore.php

--- src/neko/catcore/nekocore.php
+++ src/neko/catcore/nekocore.php
@@ -129,6 +129,10 @@ public function onSpawn(PlayerRespawnEvent $event){
                   $this->config["joinmsg"] = $args;
                   $sender->sendMessage(TextFormat::GREEN . "Set Server's join message to: " . $args);
                 return true;
+                case "facepalm":
+Server::getInstance()->broadcastMessage("§aFacepalms!");
+return true;
+			
             case "Meowinfo":
                 if (count($args) == 0 ){
                     return false;
