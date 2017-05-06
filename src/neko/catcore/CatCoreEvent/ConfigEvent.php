cf3161734ae96d6f6a4b5b81d6232c9f2ad86b29
Nekiechan <nekiechan678@gmail.com>
2017-05-06 16:30:43 -0500

    Update ConfigEvent.php

--- src/neko/catcore/CatCoreEvent/ConfigEvent.php
+++ src/neko/catcore/CatCoreEvent/ConfigEvent.php
@@ -1,10 +1,31 @@
 <?php
 namespace neko\catcore\CatCoreEvent;
-
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\event\player;
+use pocketmine\event\plugin\PluginEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\TranslationContainer;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\event\player\PlayerJoinEvent;
 class ConfigEvent extends PluginEvent {
-public function makeDir(){
+    public static $handlerList = null;
+    private $name;
+    public function __construct($plugin, $name){
+        parent::__construct($plugin);
+        $this->player = $name;
+    }
+    /**@return Player*/
+    public function getPlayer(){
+        return $this->player;
+    }
+    public function makeDir(){
         @mkdir($this->dataPath());
         @mkdir($this->dataPath()."players/");
         @mkdir($this->dataPath()."scoring/");
 }
 }
+
