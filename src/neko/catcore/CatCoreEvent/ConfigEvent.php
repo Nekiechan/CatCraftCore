72207c09d471729acda8795c4c14558c9399d6fe
Nekiechan <nekiechan678@gmail.com>
2017-05-06 16:26:00 -0500

    Update ConfigEvent.php

--- src/neko/catcore/CatCoreEvent/ConfigEvent.php
+++ src/neko/catcore/CatCoreEvent/ConfigEvent.php
@@ -1,2 +1,10 @@
 <?php
 namespace neko\catcore\CatCoreEvent;
+
+class ConfigEvent extends PluginEvent {
+public function makeDir(){
+        @mkdir($this->dataPath());
+        @mkdir($this->dataPath()."players/");
+        @mkdir($this->dataPath()."scoring/");
+}
+}
