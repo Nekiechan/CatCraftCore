bce89d6b45b442cdd95cc61402bf4615982db279
Nekiechan <nekiechan678@gmail.com>
2017-05-11 20:59:11 -0500

    Update BroadcastPluginTask.php

--- src/neko/catcore/BroadcastPluginTask.php
+++ src/neko/catcore/BroadcastPluginTask.php
@@ -6,7 +6,7 @@ use pocketmine\Server;
 class BroadcastPluginTask extends PluginTask{
 	public function onRun($currentTick){
 		if($currentTick<=250){
-		Server::getInstance()->broadcastMessage("§0[§9NekoCraft§0]§f§l Version: Neko413.1424.77356Alpha Made by NekoGirl678!");
+		Server::getInstance()->broadcastMessage("§0[§9NekoCraft§0]§f§l Version: Neko4.5.0Alpha Made by NekoGirl678!");
 		}
 		//set auto poster TODO
 		
